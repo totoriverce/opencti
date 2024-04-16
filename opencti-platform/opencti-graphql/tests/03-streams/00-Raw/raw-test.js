f4947c33898d79390f1b9a166641663784641829
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-04-16 12:28:09 -0400

    [backend] Updated hardcoded api test values

--- opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
+++ opencti-platform/opencti-graphql/tests/03-streams/00-Raw/raw-test.js
@@ -44,7 +44,7 @@ describe('Raw streams tests', () => {
       expect(createEventsByTypes.incident.length).toBe(2);
       expect(createEventsByTypes.report.length).toBe(31);
       expect(createEventsByTypes.tool.length).toBe(2);
-      expect(createEventsByTypes.vocabulary.length).toBe(335); // 328 created at init + 2 created in tests + 5 vocabulary organizations types
+      expect(createEventsByTypes.vocabulary.length).toBe(342); // 328 created at init + 2 created in tests + 5 vocabulary organizations types + 7 persona
       expect(createEventsByTypes.vulnerability.length).toBe(7);
       expect(createEvents.length).toBe(751);
       for (let createIndex = 0; createIndex < createEvents.length; createIndex += 1) {
